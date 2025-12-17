45fe924ebedad793d385357c14ba98824cb61de3
wira kusuma <kusumawira98@gmail.com>
2025-12-17 22:33:46 +0700

    feat : add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['/node_modules/'],
   testMatch: ['**/test/**/*.test.js'],
+  collectCoverageFrom: [
+    'controllers/**/*.js',
+    'models/**/*.js',
+    'routes/**/*.js',
+  ],
+  coverageDirectory: 'coverage',
   verbose: true,
   testTimeout: 10000
 };
